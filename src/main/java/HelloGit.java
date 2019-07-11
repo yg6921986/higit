7f17255e9503b461867f929eda8fba183291e20e
yg6921986 <52604538+yg6921986@users.noreply.github.com>
2019-07-11 15:39:06 +0800

    Update HelloGit.java

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("第一个ideagit");
         System.out.println("我要修改一下");
+         System.out.println("远程修改1");
     }
 }
