04c46138ee45d67ffb6b870cfa6550d9e10ef1c4
yg <892337683@qq.com>
2019-07-11 14:34:08 +0800

    第一次提交

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,11 @@
+/**
+ * @author Francis.G.Yang
+ * @version v1.0
+ * @date 2019/7/11 14:26
+ * @description TODO
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("第一个ideagit");
+    }
+}
